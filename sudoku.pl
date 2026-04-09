875cc383fb312da049d5578fbffc96b9e4b7faba
Sébastien Doussang <sebastien.doussang@epitech.eu>
2026-04-09 19:54:07 +0200

    feat: more dynamic maplist

--- sudoku.pl
+++ sudoku.pl
@@ -1,32 +1,31 @@
 :- use_module(library(clpfd)).
 
-sudoku(A1, A2, A3, A4, B1, B2, B3, B4, C1, C2, C3, C4, D1, D2, D3, D4) :-
-    Vars = [A1,A2,A3,A4,
-            B1,B2,B3,B4,
-            C1,C2,C3,C4,
-            D1,D2,D3,D4],
+assign_vals(Row) :-
+    Row ins 1..4.
 
-    Vars ins 1..4,
+sudoku(Rows) :-
+    Rows = [[A1,A2,A3,A4],
+            [B1,B2,B3,B4],
+            [C1,C2,C3,C4],
+            [D1,D2,D3,D4]],
+    
+    maplist(assign_vals, Rows),
 
     all_distinct([A1,A2,A3,A4]),
     all_distinct([B1,B2,B3,B4]),
     all_distinct([C1,C2,C3,C4]),
     all_distinct([D1,D2,D3,D4]),
+
     all_distinct([A1,B1,C1,D1]),
     all_distinct([A2,B2,C2,D2]),
     all_distinct([A3,B3,C3,D3]),
     all_distinct([A4,B4,C4,D4]),
-    all_distinct([A1,A2,B1,B2]),
-    all_distinct([A3,A4,B3,B4]),
-    all_distinct([C1,C2,D1,D2]),
-    all_distinct([C3,C4,D3,D4]),
 
+    append(Rows, Vars),
     labeling([], Vars),
 
-    printRow([A1, A2, A3, A4]),
-    printRow([B1, B2, B3, B4]),
-    printRow([C1, C2, C3, C4]),
-    printRow([D1, D2, D3, D4]).
+    maplist(printRow, Rows).
+
 
 printRow([]) :-
     nl.
