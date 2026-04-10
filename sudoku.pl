ff71397ee9d2278c1f4f29ca1593122b97d78ef5
Sébastien Doussang <sebastien.doussang@epitech.eu>
2026-04-10 10:06:28 +0200

    feat: working on NxN sudoku grid + test inputs

--- sudoku.pl
+++ sudoku.pl
@@ -1,21 +1,22 @@
 :- use_module(library(clpfd)).
 
-assign_vals(Row) :-
-    Row ins 1..4.
+make_num_matrix(N, Rows) :-
+    length(Rows, N),
+    maplist(same_length_(N), Rows).
+
+same_length_(N, Row) :-
+    length(Row, N).
 
 sudoku(Rows) :-
-    Rows = [[A1,A2,A3,A4],
-            [B1,B2,B3,B4],
-            [C1,C2,C3,C4],
-            [D1,D2,D3,D4]],
-    
-    maplist(assign_vals, Rows),
+    length(Rows, N),
+    make_num_matrix(N, Rows),
+    append(Rows, Vars),
+    Vars ins 1..N,    
 
     maplist(all_distinct, Rows),
     transpose(Rows, Cols),
     maplist(all_distinct, Cols),
 
-    append(Rows, Vars),
     labeling([], Vars),
 
     maplist(printRow, Rows).
